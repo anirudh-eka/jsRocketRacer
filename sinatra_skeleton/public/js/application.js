53224944c0d86c10242251c2899bdc85c83573a9
Apprentice <apprentice@dbc15.local>
2013-08-13 22:25:27 -0700

    clean up code

--- sinatra_skeleton/public/js/application.js
+++ sinatra_skeleton/public/js/application.js
@@ -7,11 +7,6 @@ $(document).ready(function() {
   var result_url = $('#url').text()
 
   var resetRace = function(winner, time) {
-    // $('#player2_strip td.active').removeClass('active');
-    // $('#player1_strip td.active').removeClass('active');
-    // $.post('ajax/test.html', function(data) {
-    //   $('.result').html(data);
-    // });
 
     var data = {
       winner: winner,
@@ -33,25 +28,13 @@ $(document).ready(function() {
         $('#ye .text').before(winner+" wins!")  
         $('#ye').fadeIn('slow');
       }
-      // alert(response);
     });
-    // alert(result_url)
-    // document.location.href= result_url
-    // $('#player1_strip').children().remove();
-    // $('#player2_strip').children().remove();
-
-    // $('#player2_strip').append("<td class = 'active'>"+player_avatar+"</td>");
-    // $('#player1_strip').append("<td class = 'active'>"+player_avatar+"</td>");
-    // strokes_p1 = 0; 
-    // strokes_p2 = 0;
   };
 
 
   $(document).on('keyup', function(event) {
     var startTime = new Date().getSeconds();
-    // $('td').removeClass('active')
-  // while (($('#player1_strip td.active') != $('#player1_strip').last) && ($('#player2_strip td.active') != $('#player2_strip').last)) {
-    if(event.which == 81) {
+     if(event.which == 81) {
       if (strokes_p1 < 80){
         $('#player1_strip td.active').empty().removeClass('active').after( "<td class = 'active'>"+jay_avatar+"</td>" );
         strokes_p1++;
@@ -59,7 +42,6 @@ $(document).ready(function() {
       else {
         var endTime = new Date().getSeconds();
         var totalTime = endTime - startTime;
-        alert("Player 1 wins!");
 
         resetRace("player 1", totalTime);
         
@@ -73,14 +55,9 @@ $(document).ready(function() {
       else {
         var endTime = new Date().getSeconds();
         var totalTime = endTime - startTime;
-        alert("Player 2 wins!");
         resetRace("player 2", totalTime);
-        // document.location.href='/results'
       }
     } 
 
-
-    // Detect which key was pressed and call the appropriate function
-    // Google "jquery keyup what key was pressed" if you don't know how
   });
 });
